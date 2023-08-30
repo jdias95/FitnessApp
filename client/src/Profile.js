436f4b2f94ac3953aea72426f42dd03a592e358f
Josh Dias <dias.joshua7@yahoo.com>
2023-08-30 16:02:55 -0700

    Profile component updated to allow for height and weight conversions

--- client/src/Profile.js
+++ client/src/Profile.js
@@ -5,9 +5,12 @@ const Profile = () => {
   const [userIdReg, setUserIdReg] = useState(0);
   const [weightReg, setWeightReg] = useState(0);
   const [heightReg, setHeightReg] = useState(0);
+  const [feet, setFeet] = useState(0);
+  const [inches, setInches] = useState(0);
   const [ageReg, setAgeReg] = useState(0);
   const [activityLevelReg, setActivityLevelReg] = useState("");
   const [genderReg, setGenderReg] = useState("");
+  const [measuremantType, setMeasuremantType] = useState("Imperial");
 
   useEffect(() => {
     Axios.get("http://localhost:3001/api/login").then((response) => {
@@ -55,30 +58,94 @@ const Profile = () => {
     return newInches;
   };
 
+  const caloriesBurnedMen = (weight, height, age, activityLevel) => {
+    const bmr = 66 + 6.2 * weight + 12.7 * height - 6.76 * age;
+    const total = bmr * activityLevel;
+    return total;
+  };
+
+  const caloriesBurnedWomen = (weight, height, age, activityLevel) => {
+    const bmr = 655.1 + 4.35 * weight + 4.7 * height - 4.7 * age;
+    const total = bmr * activityLevel;
+    return total;
+  };
+
   return (
     <div className="App">
       <div className="profile container">
         <div className="form">
           <h1>Profile</h1>
-          <label>Weight:</label>
-          <input
-            type="number"
+          {measuremantType == "Imperial" ? (
+            <div className="height-imperial-container">
+              <label>Weight:</label>
+              <input
+                type="number"
+                onChange={(e) => {
+                  setWeightReg(parseInt(e.target.value));
+                }}
+              />
+              <label>lbs</label>
+              <br></br>
+              <label>Height:</label>
+              <input
+                type="number"
+                onChange={(e) => {
+                  setFeet(parseInt(e.target.value));
+                  setHeightReg((prevHeight) =>
+                    convertHeightImperial(parseInt(e.target.value), inches)
+                  );
+                }}
+              />
+              <label>ft</label>
+              <input
+                type="number"
+                onChange={(e) => {
+                  setInches(parseInt(e.target.value));
+                  setHeightReg((prevHeight) =>
+                    convertHeightImperial(feet, parseInt(e.target.value))
+                  );
+                }}
+              />
+              <label>in</label>
+            </div>
+          ) : (
+            <div>
+              <label>Weight:</label>
+              <input
+                type="number"
+                onChange={(e) => {
+                  setWeightReg(convertWeight(parseInt(e.target.value)));
+                }}
+              />
+              <label>kgs</label>
+              <br></br>
+              <label>Height:</label>
+              <input
+                type="number"
+                onChange={(e) => {
+                  setHeightReg(convertHeightMetric(parseInt(e.target.value)));
+                }}
+              />
+              <label>cm</label>
+              {console.log(weightReg, heightReg)}
+            </div>
+          )}
+
+          <select
+            name="measuremantType"
+            id="measuremant-type"
             onChange={(e) => {
-              setWeightReg(e.target.value);
+              setMeasuremantType(e.target.value);
             }}
-          />
-          <label>Height:</label>
-          <input
-            type="number"
-            onChange={(e) => {
-              setHeightReg(e.target.value);
-            }}
-          />
+          >
+            <option value="Imperial">Imperial</option>
+            <option value="Metric">Metric</option>
+          </select>
           <label>Age:</label>
           <input
             type="number"
             onChange={(e) => {
-              setAgeReg(e.target.value);
+              setAgeReg(parseInt(e.target.value));
             }}
           />
           <label>Activity Level:</label>
